c7f3d9d141993605e6a39282a6022e142c82b39c
scheuringtamas <sch.tamas5@gmail.com>
2024-09-28 14:32:01 +0200

    Testing

--- main.js
+++ main.js
@@ -207,3 +207,15 @@ class LinkedList {
     return removedNode;
   }
 }
+
+// Testing
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
